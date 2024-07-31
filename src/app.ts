4a21d749ca367464adf8e083dabe25c38ecd1652
berdnaski <berdnaski1177@gmail.com>
2024-07-31 15:24:00 -0300

    route to user created

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,24 @@
+import { z } from "zod";
 import fastify from "fastify";
+import { prisma } from "./lib/prisma";
 
 export const app = fastify();
+
+app.post('/users', async(req, reply) => {
+    const registerBodySchema = z.object({
+        name: z.string().min(3).max(255),
+        email: z.string().email().min(6).max(255),
+        password: z.string().min(6),
+    })
+
+    const {  name, email, password } = registerBodySchema.parse(req.body);
+
+    await prisma.user.create({
+        data: {
+            name,
+            email,
+            password_hash: password,
+        },
+    })
+    return reply.status(201).send();
+})
